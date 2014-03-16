bba5ecccf934f57ad935e0bf959a72b34a9ef235
aokabin <mi.aoka.breakdance@gmail.com>
2014-03-17 03:44:04 +0900

    yasashisa gothic

--- test.rb
+++ test.rb
@@ -120,6 +120,7 @@ def make_card (hash)
 
   surface = Cairo::ImageSurface.new(width, height)
   context = Cairo::Context.new(surface)
+  context.select_font_face("XXXXX")
 
   # 白背景
   context.set_source_rgb(1, 1, 1)  # 色指定=白色
